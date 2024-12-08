ba3e229f1fc690923d1a530fd0a368670e74604d
Robert-Walker0 <algorithmicrealmist@gmail.com>
2024-12-07 19:06:46 -0800

    Add linking and styling for GitHub button

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
             <ul>
                 <li><img className={styles.img1} src={Screenshot}></img></li>
                 <li><p>Monster Hunter Rise Sunbreak Set Builder, currently in alpha development. If you have a suggestion or issue, let us know by opening a GitHub PR or issue! </p></li>
-                <li><img src={GitHubImg}></img></li>
+                <li><a href="https://github.com/Robert-Walker0/MHRS-Set-Builder"><img src={GitHubImg} className={styles.git}></img></a></li>
             </ul>
             <p></p>
             
