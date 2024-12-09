6039c7347f22b107482f2d6163ca0543232321dc
Robert-Walker0 <algorithmicrealmist@gmail.com>
2024-12-08 16:36:14 -0800

    Add a display bar that labels the current page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <>
     <Navbar/>
+    /* This element (main-title) didn't deserve it's own component since it was only one element by itself */
+    <h1 className="main-title">Skill Selection</h1>
     </>
   );
 }
